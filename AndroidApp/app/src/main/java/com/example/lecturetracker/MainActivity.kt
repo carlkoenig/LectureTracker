fd18f187de0c0f7b108f3c830d6c7d74a3a85541
fabiancodes <fabiancodes@gmail.com>
2022-04-13 02:29:58 +0200

    added material 3 theming & fragments

--- AndroidApp/app/src/main/java/com/example/lecturetracker/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/example/lecturetracker/MainActivity.kt
@@ -2,13 +2,46 @@ package com.example.lecturetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.lecturetracker.fragments.AccountFragment
+import com.example.lecturetracker.fragments.ManageFragment
+import com.example.lecturetracker.fragments.OverviewFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.color.DynamicColors
+import com.google.android.material.color.DynamicColorsOptions
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        DynamicColors.applyToActivitiesIfAvailable(application)
+
+
+        val overviewFragment: OverviewFragment = OverviewFragment()
+        val manageFragment: ManageFragment = ManageFragment()
+        val accountFragment: AccountFragment = AccountFragment()
+
+        setCurrentFragment(overviewFragment)
+
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.overview_button -> setCurrentFragment(overviewFragment)
+                R.id.manage_button -> setCurrentFragment(manageFragment)
+                R.id.account_button -> setCurrentFragment(accountFragment)
+            }
+            true
+        }
+
+
+
+//        val intent = Intent(this, LoginActivity::class.java)
+//        startActivity(intent)
     }
 
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
 }
---
+++ AndroidApp/app/src/main/java/com/example/lecturetracker/fragments/AccountFragment.kt
@@ -0,0 +1,26 @@
+package com.example.lecturetracker.fragments
+
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lecturetracker.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+
+class AccountFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+}
